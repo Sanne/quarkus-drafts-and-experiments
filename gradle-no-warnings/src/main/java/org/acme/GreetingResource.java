90ca3acf13f9420c4db200ae8771b55274b3d8ff
Sanne Grinovero <sanne@hibernate.org>
2025-12-19 11:51:30 +0000

    Adding a test

--- gradle-no-warnings/src/main/java/org/acme/GreetingResource.java
+++ gradle-no-warnings/src/main/java/org/acme/GreetingResource.java
@@ -5,6 +5,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
 @Path("/hello")
 public class GreetingResource {
 
@@ -13,4 +16,15 @@ public class GreetingResource {
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+    @GET
+    @Path("/flags")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getJvmFlags() {
+        List<String> inputArguments = ManagementFactory.getRuntimeMXBean().getInputArguments();
+        String joined = String.join(", ", inputArguments);
+        System.out.println(joined);
+        return joined;
+    }
+
 }
--- gradle-no-warnings/src/test/java/org/acme/GreetingResourceTest.java
+++ gradle-no-warnings/src/test/java/org/acme/GreetingResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
 class GreetingResourceTest {
@@ -15,6 +16,23 @@ class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello from Quarkus REST"));
+    }
+
+    @Test
+    void testJvmFlags() {
+        given()
+                .when().get("/hello/flags")
+                .then()
+                .statusCode(200)
+                .body(
+                        allOf(
+                                containsString("--add-opens=java.base/java.lang=ALL-UNNAMED"),
+                                containsString("--add-opens=java.base/java.lang.invoke=ALL-UNNAMED"),
+                                containsString("--add-exports=java.base/jdk.internal.module=ALL-UNNAMED"),
+                                containsString("-Djava.util.logging.manager=org.jboss.logmanager.LogManager"),
+                                containsString("-DsettingSomething=ToBlah")
+                        )
+                );
     }
 
 }
